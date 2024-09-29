5b801a924b57e9d9f0e21649ed8aec5bad8cad49
Abdullah Zia <abdullah-khan@live.co.uk>
2024-09-29 20:24:52 +0500

    first commit

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -1,27 +1,31 @@
-import { useEffect, useRef, useState } from "react";
-import "./newPrompt.css";
-import Upload from "../upload/Upload";
-import { IKImage } from "imagekitio-react";
-import model from "../../lib/gemini";
-import Markdown from "react-markdown";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useRef, useState } from 'react';
+import './newPrompt.css';
+import Upload from '../upload/Upload';
+import { IKImage } from 'imagekitio-react';
+import model from '../../lib/gemini';
+import Markdown from 'react-markdown';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const NewPrompt = ({ data }) => {
-  const [question, setQuestion] = useState("");
-  const [answer, setAnswer] = useState("");
+  const [question, setQuestion] = useState('');
+  const [answer, setAnswer] = useState('');
   const [img, setImg] = useState({
     isLoading: false,
-    error: "",
+    error: '',
     dbData: {},
     aiData: {},
   });
 
   const chat = model.startChat({
     history: [
-      data?.history.map(({ role, parts }) => ({
-        role,
-        parts: [{ text: parts[0].text }],
-      })),
+      {
+        role: 'user',
+        parts: [{ text: 'Hello, I have 2 dogs in my house.' }],
+      },
+      {
+        role: 'model',
+        parts: [{ text: 'Great to meet you. What would you like to know?' }],
+      },
     ],
     generationConfig: {
       // maxOutputTokens: 100,
@@ -32,7 +36,7 @@ const NewPrompt = ({ data }) => {
   const formRef = useRef(null);
 
   useEffect(() => {
-    endRef.current.scrollIntoView({ behavior: "smooth" });
+    endRef.current.scrollIntoView({ behavior: 'smooth' });
   }, [data, question, answer, img.dbData]);
 
   const queryClient = useQueryClient();
@@ -40,10 +44,10 @@ const NewPrompt = ({ data }) => {
   const mutation = useMutation({
     mutationFn: () => {
       return fetch(`${import.meta.env.VITE_API_URL}/api/chats/${data._id}`, {
-        method: "PUT",
-        credentials: "include",
+        method: 'PUT',
+        credentials: 'include',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           question: question.length ? question : undefined,
@@ -54,14 +58,14 @@ const NewPrompt = ({ data }) => {
     },
     onSuccess: () => {
       queryClient
-        .invalidateQueries({ queryKey: ["chat", data._id] })
+        .invalidateQueries({ queryKey: ['chat', data._id] })
         .then(() => {
           formRef.current.reset();
-          setQuestion("");
-          setAnswer("");
+          setQuestion('');
+          setAnswer('');
           setImg({
             isLoading: false,
-            error: "",
+            error: '',
             dbData: {},
             aiData: {},
           });
@@ -79,14 +83,14 @@ const NewPrompt = ({ data }) => {
       const result = await chat.sendMessageStream(
         Object.entries(img.aiData).length ? [img.aiData, text] : [text]
       );
-      let accumulatedText = "";
+      let accumulatedText = '';
       for await (const chunk of result.stream) {
         const chunkText = chunk.text();
-        console.log(chunkText);
+        console.log('chunk', chunkText);
         accumulatedText += chunkText;
+        console.log('accumulated text', accumulatedText);
         setAnswer(accumulatedText);
       }
-
       mutation.mutate();
     } catch (err) {
       console.log(err);
@@ -117,28 +121,28 @@ const NewPrompt = ({ data }) => {
   return (
     <>
       {/* ADD NEW CHAT */}
-      {img.isLoading && <div className="">Loading...</div>}
+      {img.isLoading && <div className=''>Loading...</div>}
       {img.dbData?.filePath && (
         <IKImage
           urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
           path={img.dbData?.filePath}
-          width="380"
+          width='380'
           transformation={[{ width: 380 }]}
         />
       )}
-      {question && <div className="message user">{question}</div>}
+      {question && <div className='message user'>{question}</div>}
       {answer && (
-        <div className="message">
+        <div className='message'>
           <Markdown>{answer}</Markdown>
         </div>
       )}
-      <div className="endChat" ref={endRef}></div>
-      <form className="newForm" onSubmit={handleSubmit} ref={formRef}>
+      <div className='endChat' ref={endRef}></div>
+      <form className='newForm' onSubmit={handleSubmit} ref={formRef}>
         <Upload setImg={setImg} />
-        <input id="file" type="file" multiple={false} hidden />
-        <input type="text" name="text" placeholder="Ask anything..." />
+        <input id='file' type='file' multiple={false} hidden />
+        <input type='text' name='text' placeholder='Ask anything...' />
         <button>
-          <img src="/arrow.png" alt="" />
+          <img src='/arrow.png' alt='' />
         </button>
       </form>
     </>
